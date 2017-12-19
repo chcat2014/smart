80f745b74f2c620920861f0f882b2187a34029b4
Alexey Arsenyev <alexey_arsenyev@epam.com>
2017-12-19 12:19:54 +0300

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, HashRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
 import './App.css';
 
@@ -10,13 +10,13 @@ import Test2 from './components/test1/Test1';
 class App extends Component {
   render() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Home}/>
                 <Route path='/test1' component={Test1}/>
                 <Route path='/test2' component={Test2}/>
             </Switch>
-        </HashRouter>
+        </BrowserRouter>
     );
   }
 }

7826c47c662a4628695727eda47b1dbb82a0a2f1
Alexey Arseniev <ch_cat@bk.ru>
2018-01-14 17:03:35 +0300

    design

--- src/components/test1/settings/Test1Settings.js
+++ src/components/test1/settings/Test1Settings.js
@@ -7,6 +7,7 @@ import {Link} from 'react-router-dom'
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import Toggle from 'material-ui/Toggle';
+import Paper from 'material-ui/Paper';
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 
 import './Test1Settings.css'
@@ -30,7 +31,9 @@ class Test1Settings extends Component {
           maxDigits: this.props.maxDigits,
           checkAnswers: this.props.checkAnswers,
           maxNumber: this.props.maxNumber,
-          sum: this.props.sum
+          sum: this.props.sum,
+          speedErrorText: undefined,
+          sumErrorText: undefined
         };
     }
 
@@ -40,7 +43,12 @@ class Test1Settings extends Component {
 
     onSpeedChanged(e) {
         let speed = parseFloat(e.currentTarget.value, 10);
-        this.setState({speed: speed * 1000});
+        let isCorrect = speed >= 0.1 && speed <= 10;
+
+        this.setState({
+          speed: speed * 1000,
+          speedErrorText: isCorrect ? undefined : 'Число от 0.1 до 10'
+        });
     }
 
     onSpeedClicked(e) {
@@ -71,7 +79,11 @@ class Test1Settings extends Component {
 
     onSumChanged(e) {
         let sum = parseFloat(e.currentTarget.value, 10);
-        this.setState({sum: sum});
+        const isCorrect = sum >=2 && sum <= 100;
+        this.setState({
+          sum: sum,
+          sumErrorText: isCorrect ? undefined : 'Число от 2 до 1000'
+        });
     }
 
     onCheckAnswersChanged(e, isChecked) {
@@ -92,52 +104,63 @@ class Test1Settings extends Component {
                 onClick={this.onSpeedClicked}
                 value={speed.value}
                 primary={this.state.speed === speed.value}
-                className="settingsButton"/>
+                className="Test1Settings_Button"/>
             </span>
         );
 
         return (
-            <div className="Test1Setings">
+            <div className="Test1Settings">
                 <h2>Настройки</h2>
-                <fieldset>
-                    <legend>Скорость: {this.state.speed}</legend>
-                    {contentSpeed}
-                    <TextField
-                      name="inpSpeed"
-                      id="inpSpeed"
-                      type="number"
-                      step="0.1"
-                      value={this.state.speed ? this.state.speed / 1000 : ''}
-                      errorText="!"
-                      min="0.3"
-                      max="10"
-                      required
-                      onChange={this.onSpeedChanged}
-                      className="numberField"
-                    />
-                    <span>сек</span>
-                </fieldset>
-                <fieldset>
-                    <legend>Разрядность</legend>
-                    <span>от</span>
-                    <SelectField value={this.state.minDigits} style={{width: 50}}
-                    autoWidth={true} onChange={this.onMinDigitsChanged}>
-                      <MenuItem value={1} primaryText="1" />
-                      <MenuItem value={2} primaryText="2" />
-                      <MenuItem value={3} primaryText="3" />
-                      <MenuItem value={4} primaryText="4" />
-                    </SelectField>
-                    <span>до</span>
-                    <SelectField value={this.state.maxDigits} style={{width: 50}}
-                    autoWidth={true} onChange={this.onMaxDigitsChanged}>
-                      <MenuItem value={1} primaryText="1" />
-                      <MenuItem value={2} primaryText="2" />
-                      <MenuItem value={3} primaryText="3" />
-                      <MenuItem value={4} primaryText="4" />
-                    </SelectField>
-                </fieldset>
-                <fieldset>
-                    <legend>Сложность: {this.state.complexity}</legend>
+                <Paper className="Test1Settings_Paper" zDepth={1} rounded={false} >
+                  <h4>Скорость</h4>
+                  <div style={{display: 'flex'}}>
+                    <div style={{paddingTop: 18 + 'px', flexGrow: 1}}>{contentSpeed}</div>
+                    <div style={{width: 130 + 'px', minWidth: 130 + 'px'}}>
+                        <TextField
+                          name="inpSpeed"
+                          id="inpSpeed"
+                          type="number"
+                          step="0.1"
+                          value={this.state.speed ? this.state.speed / 1000 : ''}
+                          errorText={this.state.speedErrorText}
+                          min="0.3"
+                          max="10"
+                          required
+                          onChange={this.onSpeedChanged}
+                          fullWidth={true}
+                          floatingLabelText="Секунды"
+                        />
+                    </div>
+                  </div>
+                </Paper>
+
+                <Paper className="Test1Settings_Paper" zDepth={1} rounded={false} >
+                    <h4>Разрядность</h4>
+                    <div style={{display: 'flex', paddingTop: 12}}>
+                        <div style={{padding: '16px 8px 8px 8px'}}>от</div>
+                        <div>
+                            <SelectField value={this.state.minDigits}  style={{width: 80}}
+                            onChange={this.onMinDigitsChanged}>
+                              <MenuItem value={1} primaryText="1" />
+                              <MenuItem value={2} primaryText="2" />
+                              <MenuItem value={3} primaryText="3" />
+                              <MenuItem value={4} primaryText="4" />
+                            </SelectField>
+                        </div>
+                        <div style={{padding: '16px 8px 8px 16px'}}>до</div>
+                        <div>
+                          <SelectField value={this.state.maxDigits}  style={{width: 80}}
+                          onChange={this.onMaxDigitsChanged}>
+                            <MenuItem value={1} primaryText="1" />
+                            <MenuItem value={2} primaryText="2" />
+                            <MenuItem value={3} primaryText="3" />
+                            <MenuItem value={4} primaryText="4" />
+                          </SelectField>
+                        </div>
+                    </div>
+                </Paper>
+                <Paper className="Test1Settings_Paper" zDepth={1} rounded={false}>
+                    <h4>Сложность</h4>
                     <div className="flexContainer">
                       <div style={{width: 330, marginTop: 15}}>
                           <RadioButtonGroup name="complexity"
@@ -173,37 +196,49 @@ class Test1Settings extends Component {
                         </SelectField>
                       </div>
                     </div>
-                </fieldset>
-                <fieldset>
-                  <legend>Слагаемых</legend>
-                  <TextField
-                    name="inpSum"
-                    id="inpSum"
-                    type="number"
-                    step="1"
-                    value={this.state.sum}
-                    errorText="!"
-                    min="2"
-                    max="1000"
-                    required
-                    onChange={this.onSumChanged}
-                    className="numberField"
-                  />
-                </fieldset>
-                <div style={{width: 300}}>
-                  <Toggle
-                        label="Домашняя работа"
-                        toggled={this.state.checkAnswers}
-                        onToggle={this.onCheckAnswersChanged}
-                      />
-                </div>
-                <div>
-                  <RaisedButton label="Выбрать игру"
-                    containerElement={<Link to='/'/>}
-                    className="settingsButton"/>
-                  <FloatingActionButton  onClick={this.onSettingsComplete}>
-                      <Play />
-                  </FloatingActionButton>
+                </Paper>
+
+                <Paper className="Test1Settings_Paper" zDepth={1} rounded={false}>
+                  <h4>Слагаемыe</h4>
+                  <div className="flexContainer"  style={{padding: 8}}>
+                    <div style={{padding: '15px 8px 8px'}}>Сколько цифр будем складывать</div>
+                    <div style={{width: 130}}>
+                        <TextField
+                          name="inpSum"
+                          id="inpSum"
+                          type="number"
+                          step="1"
+                          value={this.state.sum}
+                          errorText={this.state.sumErrorText}
+                          min="2"
+                          max="1000"
+                          required
+                          onChange={this.onSumChanged}
+                          fullWidth={true}
+                        />
+                      </div>
+                    </div>
+                </Paper>
+                <Paper className="Test1Settings_Paper" zDepth={1} rounded={false}>
+                  <div style={{width: 240, padding: '24px 8px'}}>
+                    <Toggle
+                          label="Домашняя работа"
+                          toggled={this.state.checkAnswers}
+                          onToggle={this.onCheckAnswersChanged}
+                        />
+                  </div>
+                </Paper>
+                <div className="flexContainer" style={{justifyContent: 'space-between'}}>
+                  <div style={{padding: 8}}>
+                      <RaisedButton label="Выбрать игру"
+                        containerElement={<Link to='/'/>}
+                        />
+                  </div>
+                  <div>
+                    <FloatingActionButton  onClick={this.onSettingsComplete}>
+                        <Play />
+                    </FloatingActionButton>
+                  </div>
                 </div>
             </div>
         );
